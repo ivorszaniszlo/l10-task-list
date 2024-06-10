81c657d8d9da2d42c9c302387d2008f069f0beae
Ivor Szaniszló <szaniszlo.ivor@gmail.com>
2024-06-10 16:17:47 +0200

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('title', 'The List of Tasks')
+
+@section('content')
+    {{-- @if (count($tasks)) --}}
+    <nav class="mb-4">
+        <a href="{{ route('tasks.create') }}"
+        class="link">Add Task!</a>
+    </nav>
+    
+    @forelse ($tasks as $task)
+        <div>
+            <a href="{{ route('tasks.show', ['task' => $task->id]) }}"
+                @class(['line-through' => $task->completed])>{{ $task->title }}</a>
+        </div>
+    @empty
+        <p>No tasks found</p>
+    @endforelse
+    {{-- @endif --}}
+
+    @if ($tasks->count())
+        <nav class="mt-4">
+            {{ $tasks->links() }}
+        </nav>
+    @endif
+@endsection
